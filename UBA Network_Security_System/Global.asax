4d595dd0396aa2b4813b2020b19f6f3c1e917475
Fecs.Prince <fecs.prince@gmail.com>
2023-01-16 14:43:41 +0100

    Add project files.

---
+++ UBA Network_Security_System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UBA_Network_Security_System.MvcApplication" Language="C#" %>
---
+++ UBA Network_Security_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UBA_Network_Security_System.Startup))]
+namespace UBA_Network_Security_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
